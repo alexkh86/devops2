6fe00f0417306eb4f7e5ddc0be774ad6708e473f
Alex Khalameizer <alex.khalameizer@gmail.com>
2025-09-10 10:15:44 +0300

    Changes to be committed: 	new file:   .idea/.gitignore 	new file:   .idea/DevOps.iml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   Dockerfile 	new file:   app.py 	new file:   requirements.txt 	new file:   test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+# age = float(input('What is your age? '))
+# if age <= 0:
+#     print('Illegal age. Quitting....')
+# else:
+#     print(f'Next year you will be {age+1} years old')
+
+# name = input('What is your name? ')
+# food = input('What is your favorite food? ')
+#
+# print(f'Your name is {name} and your favorite food is {food}')
+
+# num1 = float(input('Enter first number: '))
+# num2 = float(input('Enter second number:'))
+#
+# print(f'The sum of {num1} and {num2} is {num1+num2}')
+#
+# distance = float(input('Enter travel distance[km]: '))
+# t = float(input('Enter travel duration [h]: '))
+#
+# print(f'Average speed:{round(distance/t, 2)} kmh')
+import string
+# first = input('Enter first name: ')
+# last = input('Enter last name: ')
+#
+# print(f'Your name is {(first + ' ' + last).title()}')
+
+# sentence = input('Enter sentence: ')
+# print(f'There are {len(sentence.replace(" ", ""))} characters')
+
+prod_name = input('Product name: ')
+quantity = input('Product quantity: ')
+
+print(f'Product: {prod_name}\t|\tQty : {quantity}\t|\tTotal Qty : {quantity} ')

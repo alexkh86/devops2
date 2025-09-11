42e047e3aa50855599b0529824859b1c1a84a9dc
Alex Khalameizer <alex.khalameizer@gmail.com>
2025-09-11 13:41:36 +0300

    Changed displayed message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello from docker (after ci)!"
+    return "Ma nishma zadik"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
